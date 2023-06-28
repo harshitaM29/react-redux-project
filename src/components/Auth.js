3bc6c6e56fd070cf7ed1eeb2bf0151cc34e5958c
harshitaM29 <harshita.mundhe29@gmail.com>
2023-06-28 17:08:09 +0530

    Implmented the auth store

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -1,6 +1,16 @@
 import classes from './Auth.module.css';
+import { authActions } from '../store/index';
+import {useSelector, useDispatch} from 'react-redux'
 
 const Auth = () => {
+
+  const dispatch = useDispatch();
+  const isAuthenticated = useSelector(state => state.auth.isAuthenticated);
+  console.log(isAuthenticated)
+
+  const loginHandler=() => {
+    dispatch(authActions.login());
+  }
   return (
     <main className={classes.auth}>
       <section>
@@ -13,7 +23,7 @@ const Auth = () => {
             <label htmlFor='password'>Password</label>
             <input type='password' id='password' />
           </div>
-          <button>Login</button>
+          <button onClick={loginHandler}>Login</button>
         </form>
       </section>
     </main>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,20 +1,28 @@
 import classes from './Header.module.css';
+import {authActions} from '../store/index';
+import {useSelector, useDispatch} from 'react-redux'
 
 const Header = () => {
+  const isLoggedIn = useSelector(state => state.auth.isAuthenticated);
+  const dispatch = useDispatch();
+  const logoutHandler=() => {
+    dispatch(authActions.logout())
+  }
+
   return (
     <header className={classes.header}>
       <h1>Redux Auth</h1>
       <nav>
         <ul>
-          <li>
+         {isLoggedIn && <li>
             <a href='/'>My Products</a>
-          </li>
-          <li>
+          </li> }
+         {isLoggedIn && <li>
             <a href='/'>My Sales</a>
-          </li>
-          <li>
-            <button>Logout</button>
-          </li>
+          </li> }
+          {isLoggedIn && <li>
+            <button onClick={logoutHandler}>Logout</button>
+          </li>  }
         </ul>
       </nav>
     </header>
